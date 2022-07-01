e0c49abd86055a72f5f150c81b226bdad5743b9d
admin <admin@YD-08>
2022-07-01 17:42:58 +0900

    Fourth

--- HelloWorld/src/co/edu/Example05_2.java
+++ HelloWorld/src/co/edu/Example05_2.java
@@ -2,7 +2,7 @@ package co.edu;
 
 import java.util.Scanner;
 
-public class Example05 {
+public class Example05_2 {
 	public static void main(String[] args) {
 		
 //		String name = new String("홍길동");
@@ -19,9 +19,15 @@ public class Example05 {
 		System.out.println("두 번째 값을 입력");
 		int num2 = sn.nextInt();
 		
-		int result = Math.abs(num1 - num2); // 입력 2수의 차
+		int result; // 입력 2수의 차
 		
-		System.out.printf("입력값 %d와 %d의 차이는 %d입니다.", num1, num2, result); // 입력값 10과 20의 합은 30입니다.
+		if ( num1 > num2 ) {
+			result = num1 - num2;
+		} else {
+			result = num2 - num1;
+		}
+		
+		System.out.printf("입력값 %d, %d의 차이는 %d입니다.", num1, num2, result); // 입력값 10과 20의 합은 30입니다.
 		
 	}
 }
